26eab11f19264ea750d0bfa84e190b12475077dd
T4c0 <julianv483@gmail.com>
2023-11-20 04:54:56 -0600

    rollbar

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const path = require("path");
 
 const app = express();
 const port = 3000;
+const Rollbar = require('rollbar');
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '7e817556879d43338a6a83a4817e68c9',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 //setup static directory
 app.use(express.static(path.join(__dirname, "public")));
